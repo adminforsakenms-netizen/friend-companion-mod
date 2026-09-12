4c6e8cdc6b2ac7f35f9f4969822fe6248eb745d8
adminforsakenms-netizen <adminforsakenms@gmail.com>
2026-09-12 20:44:03 +0800

    Update ModItems.java

--- src/main/java/com/example/friendmod/ModItems.java
+++ src/main/java/com/example/friendmod/ModItems.java
@@ -3,15 +3,21 @@ package com.example.friendmod;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
 	public static Item FRIEND_TOTEM;
 
 	public static void register() {
+		Identifier id = FriendMod.id("friend_totem");
+		RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+
 		FRIEND_TOTEM = Registry.register(
 				Registries.ITEM,
-				FriendMod.id("friend_totem"),
-				new FriendTotemItem(new Item.Settings().maxCount(1))
+				key,
+				new FriendTotemItem(new Item.Settings().maxCount(1).registryKey(key))
 		);
 	}
 }
