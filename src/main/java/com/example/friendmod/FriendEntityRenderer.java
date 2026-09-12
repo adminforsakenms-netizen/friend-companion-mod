9dde631c3c38870bd740e80518fad7b4070bf2c0
adminforsakenms-netizen <adminforsakenms@gmail.com>
2026-09-12 15:44:03 +0800

    Update FriendEntityRenderer.java

--- src/main/java/com/example/friendmod/FriendEntityRenderer.java
+++ src/main/java/com/example/friendmod/FriendEntityRenderer.java
@@ -1,21 +1,34 @@
 package com.example.friendmod;
 
 import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.render.entity.BipedEntityRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.client.render.entity.state.ZombieEntityRenderState;
 import net.minecraft.util.Identifier;
 
-public class FriendEntityRenderer extends BipedEntityRenderer<FriendEntity, PlayerEntityModel<FriendEntity>> {
+public class FriendEntityRenderer extends MobEntityRenderer<FriendEntity, ZombieEntityRenderState, BipedEntityModel<ZombieEntityRenderState>> {
 
 	private static final Identifier TEXTURE = FriendMod.id("textures/entity/friend.png");
 
 	public FriendEntityRenderer(EntityRendererFactory.Context context) {
-		super(context, new PlayerEntityModel<>(context.getPart(EntityModelLayers.PLAYER), false), 0.5f);
+		super(context, new BipedEntityModel<>(context.getPart(EntityModelLayers.PLAYER)), 0.5f);
 	}
 
 	@Override
-	public Identifier getTexture(FriendEntity entity) {
+	public ZombieEntityRenderState createRenderState() {
+		return new ZombieEntityRenderState();
+	}
+
+	@Override
+	public Identifier getTexture(ZombieEntityRenderState state) {
 		return TEXTURE;
+	}
+
+	@Override
+	public void updateRenderState(FriendEntity friendEntity, ZombieEntityRenderState state, float tickDelta) {
+		super.updateRenderState(friendEntity, state, tickDelta);
+		BipedEntityRenderer.updateBipedRenderState(friendEntity, state, tickDelta);
 	}
 }
